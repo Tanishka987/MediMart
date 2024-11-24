f6c3ca34c2af7e7bb65c65e377d618242010aab9
tanishka987 <104723597+Tanishka987@users.noreply.github.com>
2024-11-24 19:49:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,7 @@ export const ProtectedRoute = ({children}) => {
 const ProtectedRouteForAdmin = ({children})=> {
   const admin = JSON.parse(localStorage.getItem('user'))
   
-  if(admin.user.email === 'tanishkapetwal@gmail.com'){
+  if(admin.user.email === 'tanishka@gmail.com'){
     return children
   }
   else{
